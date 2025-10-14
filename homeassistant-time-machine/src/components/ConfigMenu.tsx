92c812d3aac9d393fd0679feb064b6055904f112
saihgupr <121105543+saihgupr@users.noreply.github.com>
2025-10-14 08:56:23 -0400

    style: Remove custom dropdown arrow

--- homeassistant-time-machine/src/components/ConfigMenu.tsx
+++ homeassistant-time-machine/src/components/ConfigMenu.tsx
@@ -245,9 +245,6 @@ export default function ConfigMenu({ onClose, onSave, initialBackupFolderPath, i
                     WebkitAppearance: 'none',
                     MozAppearance: 'none',
                     appearance: 'none',
-                    backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="%239ca3af" width="16" height="16"><path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" /></svg>')`,
-                    backgroundRepeat: 'no-repeat',
-                    backgroundPosition: 'right 12px center',
                   }}
                 >
                   <option value="hourly">Hourly</option>
